ff27d9f44f82e0848737f28ef27caa848db23e98
Nguyễn Tri Anh Vũ <101261592+NoBugsJustFeatures@users.noreply.github.com>
2024-10-09 20:02:35 +0700

    Delete src directory

--- src/api/axiosConfig.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL:'https://b06e-24-47-242-131.ngrok-free.app',
-    headers: {"ngrok-skip-browser-warning": "true", "Access-Control-Allow-Origin": "true"}
-});
